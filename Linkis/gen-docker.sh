bbd1b7047f1cf1d0d441c91381044ddc38f95365
tbcheng89@gmail.com <tbcheng89@gmail.com>
2019-12-03 19:08:30 +0800

    fix

--- Linkis/gen-docker.sh
+++ Linkis/gen-docker.sh
@@ -1,5 +1,8 @@
-version="0.9.1"
+LINKIS_VERSION="0.9.1"
 port_begin=38180
+deployUser="hadoop"
+
+source .env
 
 echo "version: '3.6'
 services:" > docker-compose.yaml
@@ -14,7 +17,7 @@ RUN mkdir -p /opt \
 && unzip module.zip -d /opt \
 && rm -rf module.zip" > Dockerfile
 
-#docker build -t="linkis-lib-base:$version" .
+#docker build -t="linkis-lib-base:$LINKIS_VERSION" .
 
 ### 制作linkis各服务镜像
 
@@ -24,7 +27,7 @@ do
     name=${filename%%.*}
     mkdir -p $name
 
-    echo "FROM linkis-lib-base:$version
+    echo "FROM linkis-lib-base:$LINKIS_VERSION
 ADD $file /$filename
 
 RUN mkdir -p /opt && \
@@ -44,46 +47,73 @@ ENTRYPOINT [\"sh\",\"./bin/\$(ls bin|grep start)\"]
 echo "
     $name:
         environment:
+          - HADOOP_HOME=\$HADOOP_HOME
+          - HADOOP_CONF_DIR=\$HADOOP_CONF_DIR
+          - HIVE_HOME=\$HIVE_HOME
+          - HIVE_CONF_DIR=\$HIVE_CONF_DIR
+          - SPARK_HOME=\$SPARK_HOME
+          - SPARK_CONF_DIR=\$SPARK_CONF_DIR
+          - PYSPARK_ALLOW_INSECURE_GATEWAY=\$PYSPARK_ALLOW_INSECURE_GATEWAY
           - SERVICE_URL_DEFAULT_ZONE=http://eureka:8080
           - SERVER_PORT=8080
           - SERVER_HOSTNAME=0.0.0.0
           - WDS_LINKIS_LDAP_PROXY_URL=
           - WDS_LINKIS_LDAP_PROXY_BASEDN=
-          - WDS_LINKIS_GATEWAY_ADMIN_USER=hadoop
+          - WDS_LINKIS_GATEWAY_ADMIN_USER=$deployUser
           - WDS_LINKIS_SERVER_MYBATIS_DATASOURCE_URL=jdbc:mysql://mysql8:3306/linkis?characterEncoding=UTF-8
           - WDS_LINKIS_SERVER_MYBATIS_DATASOURCE_USERNAME=root
           - WDS_LINKIS_SERVER_MYBATIS_DATASOURCE_PASSWORD=abc12345
-          - WDS_LINKIS_WORKSPACE_FILESYSTEM_LOCALUSERROOTPATH=
-          - WDS_LINKIS_WORKSPACE_FILESYSTEM_HDFSUSERROOTPATH_PREFIX=
-          - WDS_LINKIS_ENGINEMANAGER_SUDO_SCRIPT=
-          - WDS_LINKIS_ENTRANCE_CONFIG_LOGPATH=
-          - WDS_LINKIS_RESULTSET_STORE_PATH=
-          - HIVE_META_URL=
-          - HIVE_META_USER=
-          - HIVE_META_PASSWORD=
-          - WDS_LINKIS_ENGINEMANAGER_CORE_JAR=
-          - WDS_LINKIS_SPARK_DRIVER_CONF_MAINJAR=
-          - WDS_LINKIS_RESULTSET_STORE_PATH=
-        image: \"$name:$version\"
+          - WDS_LINKIS_WORKSPACE_FILESYSTEM_LOCALUSERROOTPATH=file:///opt/$name/tmp/
+          - WDS_LINKIS_WORKSPACE_FILESYSTEM_HDFSUSERROOTPATH_PREFIX=\$HDFS_USER_ROOT_PATH
+          - WDS_LINKIS_ENGINEMANAGER_SUDO_SCRIPT=/opt/$name/bin/rootScript.sh
+          - WDS_LINKIS_ENTRANCE_CONFIG_LOGPATH=file:///opt/$name/tmp/
+          - WDS_LINKIS_RESULTSET_STORE_PATH=\$RESULT_SET_ROOT_PATH
+          - HIVE_META_URL=\$HIVE_META_URL
+          - HIVE_META_USER=\$HIVE_META_USER
+          - HIVE_META_PASSWORD=\$HIVE_META_PASSWORD
+          - WDS_LINKIS_ENGINEMANAGER_CORE_JAR=/opt/$name/lib/linkis-ujes-spark-engine-$LINKIS_VERSION.jar
+          - WDS_LINKIS_SPARK_DRIVER_CONF_MAINJAR=/opt/$name/conf:/opt/$name/lib/*
+          - WDS_LINKIS_RESULTSET_STORE_PATH=\$HDFS_USER_ROOT_PATH
+        image: \"$name:$LINKIS_VERSION\"
         ports:
           - $port_begin:8080
         restart: always
         volumes:
         - ${name}_logs:/opt/$name/logs
+        - ${name}_tmp:/opt/$name/tmp
+        - $HADOOP_HOME:$HADOOP_HOME
+        - $HADOOP_CONF_DIR:$HADOOP_CONF_DIR
+        - $HIVE_HOME:$HIVE_HOME
+        - $HIVE_CONF_DIR:$HIVE_CONF_DIR
+        - $SPARK_HOME:$SPARK_HOME
+        - $SPARK_CONF_DIR:$SPARK_CONF_DIR
 " >> docker-compose.yaml
 
 ((port_begin++))
 
 volumes="${name}_logs:
+    ${name}_tmp:
     $volumes"
 
-echo 'docker build -t="'$name':'$version'" -f '$name'/Dockerfile .'
-#docker build -t="$name:$version" -f $name/Dockerfile .
+echo 'docker build -t="'$name':'$LINKIS_VERSION'" -f '$name'/Dockerfile .'
+#docker build -t="$name:$LINKIS_VERSION" -f $name/Dockerfile .
 
 done
 
 echo "
+    mysql8:
+      image: mysql:8
+      restart: always
+      environment:
+        - MYSQL_ROOT_PASSWORD=abc12345
+        - MYSQL_DATABASE=linkis
+      ports:
+        - 53306:3306
+      volumes:
+        - mysql_data:/var/lib/mysql
+        - initdb:/docker-entrypoint-initdb.d:ro
 volumes:
+    mysql_data:
     $volumes
-">>docker-compose.yaml
+" >> docker-compose.yaml
 
